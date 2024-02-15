f8c4f825e9a49e1345b16eb5f826fdf56ccfd1b1
Ekaterina Aslanidi <ekaterinaaslanidi@MacBook-Air-Ekaterina.local>
2024-02-15 19:15:43 +0300

    add main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,31 @@
 import React from "react";
-import './Main.css';
+import "./Main.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import mainImage from "../../images/mainImage.png";
 
 function Main() {
-    return(
-        <>
-            <Header />
-            <h1>
-                Вдохновляем предпринимателей, поднимая их бизнес на новые вершины.
-            </h1>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <section className="main">
+        <div className="container">
+          <h1 className="title">
+            Вдохновляем предпринимателей, поднимая их бизнес на новые вершины.
+          </h1>
+          <h2 className="text">
+            Благодаря эффектному дизайну, отстройки от конкурентов и построению
+            результативной системы продаж
+          </h2>
+          <button className="button" type="button" name="button">
+            Рассчитать стоимость
+          </button>
+        </div>
+          <img className="image" src={mainImage} alt="картинка" />
+      </section>
+      <Footer />
+    </>
+  );
 }
 
 export default Main;
